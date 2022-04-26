a0b292430bc6e811b660d6b7ff4e61234fb8816e
RoCry <crysheen@gmail.com>
2022-04-26 22:28:47 +0800

    test actions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
+
 def main():
-	print("hello from actions")
-	with open("./actions.txt", "a") as f:
-		f.write(f"hello from actions {datetime.now()}\n")
+    print("hello from actions")
+    with open("./actions.txt", "a") as f:
+        f.write(f"hello from actions {datetime.now()}\n")
 
 
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
