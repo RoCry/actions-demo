017817265091540118c6d5b2f1acd6aa2758d77e
RoCry <crysheen@gmail.com>
2022-04-26 22:54:31 +0800

    test actions

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from datetime import datetime
+import feedparser
 
 
 def main():
-    print("hello from actions")
-    with open("./actions.txt", "a") as f:
-        f.write(f"hello from actions {datetime.now()}\n")
+    feed = feedparser.parse("https://hnrss.org/frontpage")
+
+    with open("./feeds.md", "w") as f:
+        f.write(f"# {feed['feed']['title']}\n\n")
+        for item in feed["entries"]:
+            title = item["title"]
+            link = item["link"]
+            f.write(f"- [{title}]({link})\n")
 
 
 if __name__ == "__main__":
