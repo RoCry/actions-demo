d56b7b4ecb747625fc63985bcee399860dadb1d5
RoCry <crysheen@gmail.com>
2022-05-19 10:55:06 +0800

    test release

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     with open("./feeds1.md", "w") as f1, open("./feeds2.md", "w") as f2:
         time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         title = f"# {feed['feed']['title']} {time_str}\n\n"
+        print(f"::set-output name=title::{title}")
         f1.write(title)
         f2.write(title)
         for item in feed["entries"]:
@@ -17,7 +18,6 @@ def main():
             link = item["link"]
             f1.write(f"- [{title}]({link})\n")
             f2.write(f"- [{title}]({link})\n")
-            print(f"::set-output name=title::{title}")
 
     body = "\n".join(map(lambda e: "- " + e["title"], feed["entries"]))
     print(f"::set-output name=body::{body}")
